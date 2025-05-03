93c3ac1c77b77f1e8384456b83f43ec58f6ef2b8
Ni But Crazy <3104955349@qq.com>
2025-05-03 11:11:37 +0800

    修复了css加载顺序问题

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router";
-import router from "./utils/router"
 import './assets/styles/index.css'
+import router from "./utils/router"
+
 
 
 createRoot(document.getElementById('root')!).render(
