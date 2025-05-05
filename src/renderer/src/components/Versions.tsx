c5e58446355f99be106349e70dec60b2b4cd8ba4
Ni But Crazy <3104955349@qq.com>
2025-05-05 21:07:53 +0800

    添加了网页运行调试的运行配置

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 
-function Versions(): React.JSX.Element {
-  const [versions] = useState(window.electron.process.versions)
+const is_electron = true ? window.electron: false
 
-  return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
-    </ul>
-  )
+function Versions(): React.JSX.Element {
+  if (is_electron) {
+    const [versions] = useState(window.electron.process.versions)
+
+    return (
+        <ul className="versions">
+          <li className="electron-version">Electron v{versions.electron}</li>
+          <li className="chrome-version">Chromium v{versions.chrome}</li>
+          <li className="node-version">Node v{versions.node}</li>
+        </ul>
+    )
+  } else {
+    return <ul className="versions"><li className="electron-version">网页开发调试中</li></ul>
+  }
 }
 
 export default Versions
